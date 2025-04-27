d83c93cbda1779b1b32d921c49fac7038f096b19
jngmnj <exorbitance@naver.com>
2025-04-27 18:31:23 +0900

    ✨ Feat: eslint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,27 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import { fixupPluginRules } from '@eslint/compat';
+import js from '@eslint/js';
+import eslintPluginImport from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+/*
+ * @eslint/compat:
+ *   구버전 스타일 플러그인을 최신 포맷에 맞게 Fixup 해주는 도구
+ *   fixupPluginRules() 함수는 플러그인 규칙(Rules)을 변환해주는 함수
+ */
+const patchedImportPlugin = fixupPluginRules(eslintPluginImport);
+const patchedReactHooksPlugin = fixupPluginRules(reactHooks);
 
 export default [
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -17,17 +30,77 @@ export default [
       },
     },
     plugins: {
-      'react-hooks': reactHooks,
+      'react-hooks': patchedReactHooksPlugin,
       'react-refresh': reactRefresh,
+      react: eslintPluginReact,
+      'jsx-a11y': jsxA11y,
+      import: patchedImportPlugin,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
+      ...eslintPluginReact.configs.recommended.rules,
+      ...patchedReactHooksPlugin.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      ...patchedImportPlugin.configs.recommended.rules,
+
+      // Airbnb 룰 핵심 추출
+      'react/jsx-filename-extension': ['warn', { extensions: ['.jsx'] }],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'jsx-a11y/anchor-is-valid': 'warn',
+      'jsx-a11y/alt-text': 'warn',
+      'import/order': [
+        'warn',
+        {
+          // ← import 순서 관리
+          groups: ['builtin', 'external', 'internal'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'no-var': 'warn', // var 사용 금지
+      'prefer-const': [
+        'error',
+        {
+          destructuring: 'all', // 배열과 객체 분해할당에도 적용
+          ignoreReadBeforeAssign: false, // 변수 선언 전에 읽히는 경우를 무시할지 여부
+        },
+      ],
+
+      // 추가된 플러그인 적용
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+          trailingComma: 'all',
+          tabWidth: 2,
+          useTabs: false,
+        },
+      ],
+
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-]
+];
