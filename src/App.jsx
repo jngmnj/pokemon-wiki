63e3fe3d14ad82b7e9a423657b05717b8baab292
jngmnj <exorbitance@naver.com>
2025-04-29 02:31:09 +0900

    💄Style: App.css 삭제 및 포켓몬 타입별 색깔 파일 추가(#4) - 포켓몬 type에 색깔정보가 api에 없어서 별도로 파일 추가 - App.css 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Detail from '@/pages/Detail';
 import Error from '@/pages/Error';
 import Home from '@/pages/Home';
 import Search from '@/pages/Search';
-import './App.css';
 
 function App() {
   return (
